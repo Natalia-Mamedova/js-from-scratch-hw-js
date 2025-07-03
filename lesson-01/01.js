4c79ac2dc39780d9b6edfa5ba0dd4fa573bdc6e3
Natalia-Mamedova <natalialis462@gmail.com>
2025-07-03 14:56:08 +0300

    first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,9 @@
+let sum = 0;
+let count = 1;
+while (count <= 20) {
+  sum = sum + count;
+  count++;
+  
+}
+
+console.log(sum)
---
+++ lesson-02/01.js
@@ -0,0 +1,26 @@
+/*
+* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+* Полученное булево значение сохраните в переменной isAccess.
+
+* Условия доступа:
+* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
+* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
+*/
+
+// тестовые данные (значения можно менять)
+const isAdmin = false
+const isVerifiedUser = true
+const hasSpecialPermission = true
+const hasTemporaryPass = false
+
+let isAccess = true
+
+// your code
+    if (isAdmin === true && hasSpecialPermission === true || 
+        isVerifiedUser === true && hasTemporaryPass === true ||
+        isAdmin === true && hasTemporaryPass === true ||
+        isVerifiedUser === true && hasSpecialPermission === true) {
+document.write('Hello')
+    } else {
+        document.write('no way')
+    }
