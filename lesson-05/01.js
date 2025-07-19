ef8da8e97d1952f40f09787842031a68e36d4fbd
Natalia-Mamedova <natalialis462@gmail.com>
2025-07-19 18:11:39 +0300

    Update 01.js

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,13 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+    if (resource === 'lumber' || resource === 'gold') {
+      game.resources [resource] += amount
+    } else {
+      console.log("Invalid resource")
+    }
+  }
 }
+game.addResource('gold', 120)
+console.log(game);
